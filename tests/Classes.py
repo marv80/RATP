1ba698b2463b9d120b6a5b002d6cd2f0b4f1494c
marv80 <mmarvintrg@gmail.com>
2018-11-13 23:52:27 +0100

    Update on RAPTOR algorithm classes

--- tests/Classes.py
+++ tests/Classes.py
@@ -1,18 +1,25 @@
 # Definition of the class stops which a represent a bus,tram,metro,train stop
 class Stop:
 
-    def __init__(self, tchangement):
-        # tchangement might replace F footpath when in the same station
-        self.tchangement = tchangement
+    def __init__(self, idstop, nomstop, latstop, longstop):
+        self.idstop = idstop    # GTFS stop ID
+        self.nomstop = nomstop  # Stop name
+        self.latstop = latstop  # Latitude stop
+        self.longstop = longstop    # Longitude stop
+        self.sroutes = []   # Routes serving the stop
+        self.nbsroutes = [] # Order of the stop in the Route use "{}" ?
+        self.stransferts = []
 
 
-class Trajet:
-    def __init__(self, tdepart, thdepart, tarrivee, tharrivee):
+class Trip:
+    def __init__(self, tdepart, thdepart, tarrive, tharrive, troute):
+        self.route = troute
         self.depart = tdepart
-        self.arrivee = tarrivee
+        self.arrivee = tarrive
         self.hdepart = thdepart
-        self.harrivee = tharrivee
-        self.stops = []
+        self.harrivee = tharrive
+        self.tstops = []
+        self.jtrips = []
 
 
 class Rounds:
@@ -22,8 +29,51 @@ class Rounds:
 
 class Journey:
     def __init__(self, jdepart, jhdepart):
-        self.trajet = []
+        self.jtrips = []
         self.depart = jdepart
         self.arrivee = float("inf")
         self.depart = jhdepart
+        self.transferts = []
+        self.PickUpS = {}
+        self.DropOffS = {}
+
+    def addtrip(self, trip):
+        self.jtrips.append(trip)
+
+    def addtransfert(self, transfert):
+        self.transferts.append(transfert)
+
+
+class Route:
+    def __init__(self, idroute, rname, rshortname):
+        self.id = idroute
+        self. rstops = []
+        self.rtrips = []
+        self.rname = rname
+        self.rshortname = rshortname
+
+    def addstop(self, stop):
+        self.rstops.append(stop)
+
+    def addtrip(self, trip):
+        self.rtrips.append(trip)
+
+
+class Transfert:
+    def __init__(self, ttime, tdepart, tarrive):
+        self.time = ttime
+        self.depart = tdepart
+        self.arrive = tarrive
+
+
+
+
+
+
+
+
+
+
+
+
 
--- tests/Raptor.py
+++ tests/Raptor.py
@@ -1,5 +1,16 @@
 import Classes.py
 
+class Raptor:
+    def __init__(self):
+        self.round = None
+        self.eap = None # Earliest Arrival Problem
+        self.markedStops = []
+        self.activeRoutes = {}
+
+
+
+
+
 
 
 
