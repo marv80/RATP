1a9fac4ef02328be4a886519b8b2962f28f155bf
nairahb <44749109+nairahb@users.noreply.github.com>
2019-03-08 10:32:19 +0100

    Update app.py

--- Naveco/app.py
+++ Naveco/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import Go
+import GTFS
+
+gtfs = GTFS.GTFS()
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -12,8 +15,22 @@ def index():
 def getvalue():
     source = request.form['source']
     destination = request.form['destination']
-    affichage(source, destination)
-    return render_template('pass.html', s=source, d=destination)
+    path = Go.astar(gtfs, source, destination)
+    strrr = "Your path start at : " + path[0].sadr
+   #  print("Your path start at : " + path[0].sadr)
+    for s in path:
+        strrr = strrr + s.nomstop
+        # print(s.nomstop)
+        if s.actual_route:
+           strrr = strrr + "On line : "
+           # print("On line : ")
+           strrr = strrr + s.actual_route.rshortname
+           # print(s.actual_route.rshortname)
+    print(strrr)
+
+   # return render_template('pass.html', s=source, d=destination)
+    return render_template('pass.html', s= strrr)
+
 
 def affichage (source,destination):
     print("la source est",source, "la destination", destination)
