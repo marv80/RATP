902f0b7b9133fb7bb95202b09350d809781684db
nairahb <44749109+nairahb@users.noreply.github.com>
2019-03-17 13:01:31 +0100

    Add files via upload
    Need to modify def get_stop() and def get_line():

--- Naveco/app.py
+++ Naveco/app.py
@@ -29,15 +29,39 @@ def getvalue():
     print(strrr)
 
    # return render_template('pass.html', s=source, d=destination)
-    return render_template('pass.html', s= strrr)
+    return render_template('result_path.html', s= strrr)
 
 
-def affichage (source,destination):
-    print("la source est",source, "la destination", destination)
+def affichage (source, destination): print("la source est", source, "la destination", destination)
 
-#@app.route("/next")
-#def suite():
-#    return render_template("page_suivante.html")
+@app.route("/get_line")
+def suite():
+    return render_template("get_line.html")
+
+@app.route('/get_line', methods=['POST'])
+def get_line():
+    line = request.form['get_line']
+    for r in gtfs.gtfs_routes:
+        if r.rshortname == line:
+           str_line = "" + r.rname
+        for s in r.rstops:
+            str_line = str_line + s.nomstop
+    return render_template('result_line.html', s= str_line)
+
+@app.route("/get_stop")
+def suite_stop():
+    return render_template("get_stop.html")
+
+@app.route('/get_stop', methods=['POST'])
+def get_stop():
+    stop = request.form['get_stop']
+    for s in gtfs.gtfs_stops:
+        if s.nomstop == stop:
+            str = s.nomstop
+            #print(s.nomstop)
+           # for r in s.sroutes:
+            #    str ="Ligne : " + r.rshortname + r.rname
+    return render_template('result_stop.html', s=str)
 
 
 if __name__ == '__main__':
